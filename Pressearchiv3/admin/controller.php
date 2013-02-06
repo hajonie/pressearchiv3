00365f3b89975729211149390654c6fe99eea529
hajonie <Hans-Joachim Niemann@192.168.6.200>
2013-02-06 10:32:40 +0100

    Anpassung an Pressearchiv

--- Pressearchiv3/admin/controller.php
+++ Pressearchiv3/admin/controller.php
@@ -7,13 +7,13 @@
 defined('_JEXEC') or die;
 
 /**
- * Weblinks Weblink Controller
+ * Pressearchiv Controller
  *
  * @package     Joomla.Administrator
- * @subpackage  com_weblinks
- * @since       1.5
+ * @subpackage  com_pressearchiv
+ * @since       3.0
  */
-class WeblinksController extends JControllerLegacy
+class PressearchivController extends JControllerLegacy
 {
 	/**
 	 * Method to display a view.
@@ -26,18 +26,18 @@ class WeblinksController extends JControllerLegacy
 	 */
 	public function display($cachable = false, $urlparams = false)
 	{
-		require_once JPATH_COMPONENT.'/helpers/weblinks.php';
+		require_once JPATH_COMPONENT.'/helpers/pressearchiv.php';
 
-		$view   = $this->input->get('view', 'weblinks');
+		$view   = $this->input->get('view', 'pressearchiv');
 		$layout = $this->input->get('layout', 'default');
 		$id     = $this->input->getInt('id');
 
 		// Check for edit form.
-		if ($view == 'weblink' && $layout == 'edit' && !$this->checkEditId('com_weblinks.edit.weblink', $id)) {
+		if ($view == 'pressearchiv' && $layout == 'edit' && !$this->checkEditId('com_pressearchiv.edit.pressearchiv', $id)) {
 			// Somehow the person just went to the form - we don't allow that.
 			$this->setError(JText::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id));
 			$this->setMessage($this->getError(), 'error');
-			$this->setRedirect(JRoute::_('index.php?option=com_weblinks&view=weblinks', false));
+			$this->setRedirect(JRoute::_('index.php?option=com_pressearchiv&view=pressearchivs', false));
 
 			return false;
 		}
