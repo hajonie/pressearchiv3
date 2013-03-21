72789a45807f6a1224f8e5d06211721f6d40aa80
hajonie <Hans-Joachim Niemann@HAPC088.ascherl.local>
2013-03-21 10:50:48 +0100

    Helper Datei angepasst

--- Pressearchiv3/admin/helpers/pressearchiv.php
+++ Pressearchiv3/admin/helpers/pressearchiv.php
@@ -16,61 +16,37 @@ defined('_JEXEC') or die;
  * Pressearchivs helper.
  *
  * @package     Joomla.Administrator
- * @subpackage  com_pressearchivs
- * @since       1.6
+ * @subpackage  com_pressearchiv
+ * @since       3.0
  */
-class PressearchivsHelper
+class PressearchivHelper
 {
-	/**
-	 * Configure the Linkbar.
-	 *
-	 * @param   string	The name of the active view.
-	 * @since   1.6
-	 */
-	public static function addSubmenu($vName = 'pressearchivs')
-	{
-		JHtmlSidebar::addEntry(
-		JText::_('COM_PRESSEARCHIVS_SUBMENU_PRESSEARCHIVS'),
-		'index.php?option=com_pressearchivs&view=pressearchivs',
-		$vName == 'pressearchivs'
-				);
-				JHtmlSidebar::addEntry(
-				JText::_('COM_PRESSEARCHIVS_SUBMENU_CATEGORIES'),
-				'index.php?option=com_categories&extension=com_pressearchivs',
-				$vName == 'categories'
-						);
-						if ($vName == 'categories')
-						{
-							JToolbarHelper::title(
-							JText::sprintf('COM_CATEGORIES_CATEGORIES_TITLE', JText::_('com_pressearchivs')),
-							'pressearchivs-categories');
-						}
-	}
+	public static $extension = 'com_pressearchiv';
 
 	/**
 	 * Gets a list of the actions that can be performed.
 	 *
-	 * @param   integer  The category ID.
+	 * @param   integer  $pressearchivId  The Press Archiv ID.
+	 *
 	 * @return  JObject
-	 * @since   1.6
+	 *
+	 * @since   4.0
 	 */
-	public static function getActions($categoryId = 0)
+	public static function getActions($imprintId = 0)
 	{
 		$user	= JFactory::getUser();
 		$result	= new JObject;
 
-		if (empty($categoryId))
+		if (empty($pressearchivId))
 		{
-			$assetName = 'com_pressearchivs';
-			$level = 'component';
+			$assetName = 'com_pressearchiv';
 		}
 		else
 		{
-			$assetName = 'com_pressearchivs.category.'.(int) $categoryId;
-			$level = 'category';
+			$assetName = 'com_pressearchiv.pressearchiv.' . (int) $pressearchivId;
 		}
 
-		$actions = JAccess::getActions('com_pressearchivs', $level);
+		$actions = JAccess::getActions('com_pressearchiv', 'component');
 
 		foreach ($actions as $action)
 		{
@@ -78,5 +54,83 @@ class PressearchivsHelper
 		}
 
 		return $result;
+	}
+
+	/**
+	 * Method to generate icons like in the Control Panel
+	 * originally by Jan Pavelka (Phoca.cz)
+	 *
+	 * @param   string  $link   the target
+	 * @param   string  $image  the image name releative to assets folder
+	 * @param   string  $text   the text to display
+	 *
+	 * @return  object
+	 */
+	public static function quickIconButton($link, $image, $text)
+	{
+		$imgUrl = '../media/com_pressearchiv/images/assets/';
+		$code	= '<div class="thumbnails ph-icon">'
+				. '<a class="thumbnail ph-icon-inside" href="' . $link . '">'
+						. JHTML::_('image', $imgUrl . $image, $text)
+						. '<br /><span>' . $text . '</span></a></div>' . "\n";
+
+		return $code;
+	}
+
+	/**
+	 * Get Links
+	 *
+	 * @return null
+	 */
+	public static function getLinks()
+	{
+		return null;
+	}
+
+	/**
+	 * Method to get the version of the installed press archiv.
+	 *
+	 * @author	hajonie
+	 * @since	3.0
+	 * @return	string	The version of the installed imprint.
+	 */
+	public static function getVersion()
+	{
+		$db = JFactory::getDbo();
+		$db->setQuery('SELECT manifest_cache FROM #__extensions WHERE name = "com_pressearchiv"');
+		$manifest = json_decode( $db->loadResult(), true );
+		return $manifest['version'];
+	}
+
+	/**
+	 * Configure the Linkbar
+	 *
+	 * @param   string     $vName    The current view name
+	 *
+	 * @return  void
+	 * @since   3.0
+	 */
+	public static function addSubmenu($vName)
+	{
+		JHtmlSidebar::addEntry(
+		JText::_('COM_PRESSEARCHIV_SUBMENU_CPANEL'),
+		'index.php?option=com_pressearchiv&view=cpanel',
+		$vName == 'cpanel'
+				);
+				JHtmlSidebar::addEntry(
+				JText::_('COM_PRESSEARCHIV_SUBMENU_ARTICLES'),
+				'index.php?option=com_pressearchiv&view=articles',
+				$vName == 'articles'
+						);
+						JHtmlSidebar::addEntry(
+						JText::_('COM_PRESSEARCHIV_SUBMENU_SOURCES'),
+						'index.php?option=com_pressearchiv&view=sources',
+						$vName == 'sources'
+								);
+								JHtmlSidebar::addEntry(
+								JText::_('COM_PRESSEARCHIV_SUBMENU_ABOUT'),
+								'index.php?option=com_pressearchiv&view=about',
+								$vName == 'about'
+										);
 	}
 }
