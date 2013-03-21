472eb047ad5961947775fb8fea15cde6a1fcd10a
hajonie <Hans-Joachim Niemann@HAPC088.ascherl.local>
2013-03-21 08:18:31 +0100

    SQL Dateien

---
+++ Pressearchiv3/admin/sql/uninstall.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS `#__pressearchiv_articles`;
+DROP TABLE IF EXISTS `#__pressearchiv_sources`;
+DROP TABLE IF EXISTS `#__pressearchiv_categories`;
+DROP TABLE IF EXISTS `#__pressearchiv_settings`;
