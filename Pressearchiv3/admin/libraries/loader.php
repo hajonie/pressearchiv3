18c0b8d49b48a5d1564e5fea03652425c1b77c36
hajonie <Hans-Joachim Niemann@HAPC088.ascherl.local>
2013-04-15 14:34:39 +0200

    Submenu erweitert / Ordnerstuktur erweitert

---
+++ Pressearchiv3/admin/libraries/loader.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Pressearchiv - A Joomla! Component
+ * @version     3.0.0
+ * @filename    loader.php
+ * @author      Hans-Joachim Niemann
+ * @date        15.04.2013
+ *
+ * @copyright   Copyright (C) 2011 - 2013 Pressearchiv Development Team, Inc. All rights reserved.
+ * @license     GNU General Public License version 3 or later
+ */
+
--- Pressearchiv3/admin/views/cpanel/tmpl/default.php
+++ Pressearchiv3/admin/views/cpanel/tmpl/default.php
@@ -14,7 +14,9 @@ defined('_JEXEC') or die;
 
 ?>
 <form action="index.php" method="post" name="adminForm">
-	<div id="j-sidebar-container" class="span2"><?php echo JHtmlSidebar::render(); ?></div>
+	<div id="j-sidebar-container" class="span2">
+		<?php echo JHtmlSidebar::render(); ?>
+	</div>
 	<div id="j-main-container" class="span10">
 		<div class="adminform">
 			<div class="pressearchiv-cpanel-left">
@@ -24,10 +26,10 @@ defined('_JEXEC') or die;
 					echo PressearchivHelper::quickIconButton($link, "icon-48-home.png", JText::_('COM_PRESSEARCHIV_ARTIKEL'));
 
 					$link = 'index.php?option=com_pressearchiv&amp;view=categories';
-					echo PressearchivHelper::quickIconButton($link, "icon-48-category.png", JText::_('COM_PRESSEARCHIV_KATEGORIEN'));
+					echo PressearchivHelper::quickIconButton($link, "icon-48-categories.png", JText::_('COM_PRESSEARCHIV_KATEGORIEN'));
 						
 					$link = 'index.php?option=com_pressearchiv&amp;view=sources';
-					echo PressearchivHelper::quickIconButton($link, "icon-48-source.png", JText::_('COM_PRESSEARCHIV_QUELLEN'));
+					echo PressearchivHelper::quickIconButton($link, "icon-48-sources.png", JText::_('COM_PRESSEARCHIV_QUELLEN'));
 					?>
 					<div style="clear:both">&nbsp;</div>
 				</div>
