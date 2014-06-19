eada55c4140e17177b30633a0f44e8c517dbf78f
hajonie <Niemann@HAPC088.ascherl.local>
2014-06-19 09:25:47 +0200

    View Configuration

--- Pressearchiv3/admin/views/configuration/tmpl/default.php
+++ Pressearchiv3/admin/views/configuration/tmpl/default.php
@@ -10,3 +10,23 @@
  * @license     GNU General Public License version 3 or later
  */
 
+defined('_JEXEC') or die;
+?>
+
+<form action="index.php" method="post" name="adminForm">
+	<div id="j-sidebar-container" class="span2"><?php echo JHtmlSidebar::render(); ?></div>
+	<div id="j-main-container" class="span10">
+		<div class="adminform">
+			<div class="pressearchiv-cpanel-left">
+				<div id="cpanel">
+					<h3><?php echo JText::_('JHELP');?></h3>
+					<p>
+						<a href="https://github.com/hajonie/pressearchiv3" target="_blank">
+							<?php echo JText::_('COM_PRESSEARCHIV') . ' - ' . JText::_('COM_PRESSEARCHIV_HOME');?>
+						</a>
+					</p>				
+				</div>
+			</div>
+		</div>
+	</div>
+</form>
--- Pressearchiv3/admin/views/configuration/view.html.php
+++ Pressearchiv3/admin/views/configuration/view.html.php
@@ -10,3 +10,40 @@
  * @license     GNU General Public License version 3 or later
  */
 
+defined('_JEXEC') or die;
+
+class PressearchivViewConfiguration extends JViewLegacy
+{
+	public function display($tpl = null)
+	{
+		JHTML::_('behavior.tooltip');
+
+		// Check for errors
+		if (count($errors = $this->get('Errors')))
+		{
+			JError::raiseError(500, implode('\n', $errors));
+
+			return false;
+		}
+
+		PressearchivHelper::addSubmenu('configuration');
+
+		$this->addToolBar();
+
+		parent::display($tpl);
+	}
+
+	protected function addToolBar()
+	{
+		$canDo = PressearchivHelper::getActions();
+		JToolBarHelper::title(JText::_('COM_PRESSEARCHIV') . ':' . JText::_('COM_PRESSEARCHIV_KONFIGURATION'), 'wrench');
+
+		if ($canDo->get('core.admin'))
+		{
+			JToolBarHelper::preferences('com_pressearchiv');
+			JToolBarHelper::divider();
+		}
+
+		JToolBarHelper::help('screen.pressearchiv', true);
+	}
+}
